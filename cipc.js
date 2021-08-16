50a8dff500f0b544fc1a16c55f6e6ac1d08c94e0
linhtpham09 <84039367+linhtpham09@users.noreply.github.com>
2021-08-16 15:31:16 -0400

    Add files via upload

---
+++ cipc.js
@@ -0,0 +1,12 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 2000,
+    reset: true
+});
+
+sr.reveal(`#op1, #op2, #op3, #op4, #op5, #op6, #op7, #op8, #op9, #op10`, {
+    interval: 200
+})
+
+
